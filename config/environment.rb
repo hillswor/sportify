da07b912adbfcd7c9d32dc371306d68d437c7e30
Bruce Hillsworth <brucehillsworth@Bruces-MacBook-Air.local>
2019-06-05 14:24:56 -0600

    Added to environment

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require "nokogiri"
+require "open-uri"
+require "bundler"
+
+require_relative "../lib/cli/cli"
+require_relative "../lib/leagues/mlb_teams"
+require_relative "../lib/scrapers/mlb_scraper"
